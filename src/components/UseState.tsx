963648e231ebccd4acf8dbe49bbecd403f082e46
Sri Krishna Chaithanya Raj Masimukku <mskchaithanyaraj@gmail.com>
2025-01-17 09:28:03 +0530

    Use State Hook Practice

--- src/components/UseState.tsx
+++ src/components/UseState.tsx
@@ -1,9 +1,38 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { Minus, Plus } from "lucide-react";
 
 interface UseStateProps {}
 
 const UseState: FC<UseStateProps> = ({}) => {
-  return <div>UseState</div>;
+  const [count, setCount] = useState(() => {
+    return 0;
+  });
+
+  const IncrementCounter = () => {
+    setCount((prevState) => prevState + 1);
+  };
+  const DecrementCounter = () => {
+    setCount((prevState) => prevState - 1);
+  };
+  return (
+    <div>
+      <h1>useState in React hooks</h1>
+      <p>
+        The useState hook in React enables state management within functional
+        components, allowing developers to declare and update state variables
+        seamlessly.
+      </p>
+      <h3>Implementing useState hook</h3>
+      <p>Counter</p>
+      <button onClick={DecrementCounter}>
+        <Minus />
+      </button>
+      <p>{count}</p>
+      <button onClick={IncrementCounter}>
+        <Plus />
+      </button>
+    </div>
+  );
 };
 
 export default UseState;
